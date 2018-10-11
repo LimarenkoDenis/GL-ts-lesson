98dc72f45b79fa1edfe0eeef398949f8e72e84cb
Andrew Randan <andrew.randan@mabbly.com>
2018-10-11 19:51:03 +0300

    1 done

--- src/rudai.andrey/homework.spec.ts
+++ src/rudai.andrey/homework.spec.ts
@@ -1,4 +1,4 @@
-import { sum } from './homework';
+import { countBy, sum } from './homework';
 
 test('basic', () => {
   expect(sum(1, 2, 3, 4)).toBe(10);
@@ -6,4 +6,14 @@ test('basic', () => {
 
 test('basic again', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+describe('countBy', () => {
+  test('basic', () => {
+    expect(countBy(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  test('basic again', () => {
+    expect(countBy(2, 5)).toEqual([2, 4, 6, 8, 10]);
+  });
 });
--- src/rudai.andrey/homework.ts
+++ src/rudai.andrey/homework.ts
@@ -1,2 +1,13 @@
 export const sum: (...a: number[]) => number
   = (...a: number[]) => a.reduce((acc: number, val: number) => acc + val, 0);
+
+// tslint:disable-next-line:typedef
+export const countBy = (multiple: number, length: number): number[] => {
+  const countByArr: number[] = [];
+  for (let i: number = 0; countByArr.length < length; i++) {
+    if (i % multiple === 0 && i > 0) {
+      countByArr.push(i);
+    }
+  }
+  return countByArr;
+};
