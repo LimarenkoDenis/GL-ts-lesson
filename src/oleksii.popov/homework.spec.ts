d50ac6207cd32e6a1f272daba81398522807a2bc
Oleksiy Popov <popovbecks@mail.ru>
2018-10-12 01:27:55 +0300

    Homework 1 has done (#7)
    * Homework 1 has done
    
    * refactored code
    
    * some refactoring

---
+++ src/oleksii.popov/homework.spec.ts
@@ -0,0 +1,27 @@
+import { addClass, camelizeProperty, removeClass, sum } from './homework';
+
+describe('sum', () => {
+  test('basic', () => {
+    expect(sum(1, 2, 3, 4)).toBe(10);
+  });
+
+  test('basic again', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+
+describe('addClass', () => {
+  test('addClass', () => {
+    expect(addClass({ className: 'open new' }, 'class')).toEqual({ className: 'open new class' })
+  });
+});
+describe('Camelize', () => {
+  test('camelize', () => {
+    expect(camelizeProperty('border-radius')).toBe('borderRadius')
+  })
+});
+describe('removeClass', () => {
+  test('removeClass', () => {
+    expect(removeClass({ className: 'add new class' }, 'class')).toEqual({ className: 'add new' });
+  })
+});
---
+++ src/oleksii.popov/homework.ts
@@ -0,0 +1,25 @@
+export const sum: (...a: number[]) => number
+  = (...a: number[]) => a.reduce((acc: number, val: number) => acc + val, 0);
+
+export const addClass: (obj: { className: string }, cls: string)
+  => { className: string } = (obj: { className: string }, cls: string) => {
+    if (obj.className.indexOf(cls) === -1) {
+      obj.className = obj.className + ' ' + cls;
+    }
+    return obj;
+  }
+export const camelizeProperty: (property: string) => string = (property: string) => {
+  let string = property.split('-');
+  for (let i = 1; i < string.length; i++) {
+    string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+  }
+
+  return string.join('');
+}
+export const removeClass: (obj: { className: string }, cls: string) =>
+  { className: string } = (obj: { className: string }, cls: string) => {
+    if (obj.className.indexOf(cls) !== -1) {
+      obj.className = obj.className.split(' ').filter((item: string) => item !== cls).join(' ');
+    }
+    return obj;
+  };
