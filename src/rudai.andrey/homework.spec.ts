e770b5398d769478437c2c70c1494a791ad49244
Andrew Randan <andrew.randan@mabbly.com>
2018-10-12 19:13:33 +0300

    done

--- src/rudai.andrey/homework.spec.ts
+++ src/rudai.andrey/homework.spec.ts
@@ -1,4 +1,4 @@
-import { biggestSalary, countBy, salariesSum, sum } from './homework';
+import { addClass, biggestSalary, countBy, multiplyTwo, NSO, salariesSum, SO, sum } from './homework';
 
 describe('sum', () => {
   test('basic', () => {
@@ -53,11 +53,11 @@ describe('biggestSalary', () => {
     expect(biggestSalary(salaries)).toBe('Вася');
   });
 
-  // test('basic again for empty', () => { // TODO: return after fixes
-  //   const salaries: { [key: string]: number } = {};
+  test('basic again for empty', () => {
+    const salaries: { [key: string]: number } = {};
 
-  //   expect(biggestSalary(salaries)).toBe('нет сотрудников');
-  // });
+    expect(biggestSalary(salaries)).toBe('нет сотрудников');
+  });
 });
 
 describe('countBy', () => {
@@ -67,5 +67,53 @@ describe('countBy', () => {
 
   test('basic again', () => {
     expect(countBy(2, 5)).toEqual([2, 4, 6, 8, 10]);
+  });
+});
+
+describe('addClass', () => {
+  const obj: SO = {
+    className: 'open menu'
+  };
+
+  test('basic', () => {
+    expect(addClass(obj, 'new')).toEqual('open menu new');
+  });
+
+  test('basic again', () => {
+    expect(addClass(obj, 'menu')).toEqual('open menu new');
+  });
+});
+
+describe('multiplyTwo', () => {
+  const menu1: NSO = {
+    width: 200,
+    height: 300,
+    title: 'My menu'
+  };
+
+  const menuResult1: NSO = {
+    width: 400,
+    height: 600,
+    title: 'My menu'
+  };
+
+  const menu2: NSO = {
+    width: 250,
+    height: 350,
+    title: 'My menu'
+  };
+
+  const menuResult2: NSO = {
+    width: 500,
+    height: 700,
+    title: 'My menu'
+  };
+
+  test('basic', () => {
+    expect(multiplyTwo(menu1)).toEqual(menuResult1);
+  });
+
+  test('basic again', () => {
+    expect(multiplyTwo(menu2)).toEqual(menuResult2);
   });
 });
--- src/rudai.andrey/homework.ts
+++ src/rudai.andrey/homework.ts
@@ -1,27 +1,41 @@
-// import _isEmpty from 'lodash/isEmpty'; // TODO: find solution
+export type SO = { [key: string]: string };
+export type NO = { [key: string]: number };
+export type NSO = { [key: string]: number | string };
+
+function isEmpty(n: NO): boolean {
+  if (Object.keys(n).length === 0) {
+    return true;
+  }
+  return false;
+}
+
+function isNumeric(n: string | number): boolean {
+  if (typeof n === 'number') {
+    return true;
+  }
+  return false;
+}
 
 export const sum: (...a: number[]) => number
   = (...a: number[]) => a.reduce((acc: number, val: number) => acc + val, 0);
 
-export const salariesSum: (salaries: { [key: string]: number }) => number
-  = (salaries: { [key: string]: number }) =>
+export const salariesSum: (salaries: NO) => number
+  = (salaries: NO) =>
   Object.keys(salaries).reduce((accum: number, key: string) => accum + salaries[key], 0);
 
-
-export const biggestSalary: (salaries: { [key: string]: number }) => string
-  = (salaries: { [key: string]: number }) => {
+export const biggestSalary: (salaries: NO) => string
+  = (salaries: NO) => {
     let max: number = 0;
     let employee: string = '';
-    // if (_.isEmpty(salaries)) { // TODO: fix this
-    //   employee = 'нет сотрудников';
-    // } else {
-      for (const key in salaries) {
-        if (salaries.hasOwnProperty(key) && salaries[key] > max) {
-          max = salaries[key];
-          employee = key;
-        }
+    if (isEmpty(salaries)) {
+      return 'нет сотрудников';
+    }
+    for (const key in salaries) {
+      if (salaries.hasOwnProperty(key) && salaries[key] > max) {
+        max = salaries[key];
+        employee = key;
       }
-    // }
+    }
     return employee;
   };
 
@@ -33,3 +47,26 @@ export const countBy: (...a: number[]) => number[]
     }
     return countByArr;
   };
+
+export function addClass(classObj: SO, newClass: string): string {
+  const classArray: string[] = classObj.className.split(' ');
+  for (let i: number = 0; i < classArray.length - 1; i++) {
+    if (classArray[i] === newClass) {
+      return classObj.className;
+    }
+  }
+  classArray.push(newClass);
+  const classString: string = classArray.join(' ');
+  classObj.className = classString;
+  return classObj.className;
+}
+
+export function multiplyTwo(someObject: NSO): NSO {
+  return Object.keys(someObject).reduce((acc: NSO, key: string) => {
+    const value: string | number = someObject[key];
+    if (isNumeric(value)) {
+      return { ...acc, ...{ [key]: (value as number) * 2 } };
+    }
+    return { ...acc, ...{ [key]: value } };
+  }, {});
+}
