1f8441fcf3cf34560bb02b4ec8a5de22c7eac3d3
Andrew Randan <andrewrandan@gmail.com>
2018-10-12 03:03:04 +0300

    some issues

--- src/rudai.andrey/homework.spec.ts
+++ src/rudai.andrey/homework.spec.ts
@@ -1,11 +1,63 @@
-import { countBy, sum } from './homework';
+import { biggestSalary, countBy, salariesSum, sum } from './homework';
 
-test('basic', () => {
-  expect(sum(1, 2, 3, 4)).toBe(10);
+describe('sum', () => {
+  test('basic', () => {
+    expect(sum(1, 2, 3, 4)).toBe(10);
+  });
+
+  test('basic again', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
 
-test('basic again', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('salariesSum', () => {
+  test('basic', () => {
+    const salaries: { [key: string]: number } = {
+      Вася: 100,
+      Петя: 300,
+      Даша: 250
+    };
+
+    expect(salariesSum(salaries)).toBe(650);
+  });
+
+  test('basic again', () => {
+    const salaries: { [key: string]: number } = {
+      Вася: 25,
+      Петя: 25,
+      Даша: 25
+    };
+
+    expect(salariesSum(salaries)).toBe(75);
+  });
+});
+
+describe('biggestSalary', () => {
+  test('basic', () => {
+    const salaries: { [key: string]: number } = {
+      Вася: 100,
+      Петя: 300,
+      Даша: 250
+    };
+
+    expect(biggestSalary(salaries)).toBe('Петя');
+  });
+
+  test('basic again', () => {
+    const salaries: { [key: string]: number } = {
+      Вася: 26,
+      Петя: 25,
+      Даша: 25
+    };
+
+    expect(biggestSalary(salaries)).toBe('Вася');
+  });
+
+  // test('basic again for empty', () => { // TODO: return after fixes
+  //   const salaries: { [key: string]: number } = {};
+
+  //   expect(biggestSalary(salaries)).toBe('нет сотрудников');
+  // });
 });
 
 describe('countBy', () => {
--- src/rudai.andrey/homework.ts
+++ src/rudai.andrey/homework.ts
@@ -1,13 +1,35 @@
+// import _isEmpty from 'lodash/isEmpty'; // TODO: find solution
+
 export const sum: (...a: number[]) => number
   = (...a: number[]) => a.reduce((acc: number, val: number) => acc + val, 0);
 
-// tslint:disable-next-line:typedef
-export const countBy = (multiple: number, length: number): number[] => {
-  const countByArr: number[] = [];
-  for (let i: number = 0; countByArr.length < length; i++) {
-    if (i % multiple === 0 && i > 0) {
-      countByArr.push(i);
+export const salariesSum: (salaries: { [key: string]: number }) => number
+  = (salaries: { [key: string]: number }) =>
+  Object.keys(salaries).reduce((accum: number, key: string) => accum + salaries[key], 0);
+
+
+export const biggestSalary: (salaries: { [key: string]: number }) => string
+  = (salaries: { [key: string]: number }) => {
+    let max: number = 0;
+    let employee: string = '';
+    // if (_.isEmpty(salaries)) { // TODO: fix this
+    //   employee = 'нет сотрудников';
+    // } else {
+      for (const key in salaries) {
+        if (salaries.hasOwnProperty(key) && salaries[key] > max) {
+          max = salaries[key];
+          employee = key;
+        }
+      }
+    // }
+    return employee;
+  };
+
+export const countBy: (...a: number[]) => number[]
+  = (multiple: number, length: number) => {
+    const countByArr: number[] = [];
+    for (let i: number = 0; countByArr.length < length; i++) {
+      i % multiple === 0 && i > 0 && countByArr.push(i);
     }
-  }
-  return countByArr;
-};
+    return countByArr;
+  };
