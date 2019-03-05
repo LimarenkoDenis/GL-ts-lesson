68417c5adc4e9a22ac8efd017522e2b294813ff2
Dr. Iaroslav G. Zhbankov <yzhbankov@gmail.com>
2019-03-05 12:21:48 +0200

    add first lesson homework (#19)

---
+++ src/iaroslav.zhbankov/models.ts
@@ -0,0 +1,11 @@
+export interface ISalary {
+    [key: string]: number;
+}
+
+export interface IStringNumberObject {
+    [key: string]: any;
+}
+
+export interface IObjectWithClass {
+    [key: string]: string;
+}
