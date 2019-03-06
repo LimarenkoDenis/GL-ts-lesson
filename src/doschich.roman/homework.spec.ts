d23d07b479ff99392253f399039dd5f5a990ac16
Roman Doschich <roman.doschich@gmail.com>
2019-03-06 18:45:52 +0200

    chai to jest

--- src/doschich.roman/homework.spec.ts
+++ src/doschich.roman/homework.spec.ts
@@ -1,6 +1,6 @@
 import { sum, calculateSum, findMax, multiplyNumeric, menuType, countBy, addClass, classType } from './homework';
-import { expect } from 'chai';
-import 'mocha';
+// import { expect } from 'chai';
+// import 'mocha';
 
 const salaries: { [key: string]: number } = {
   "Вася": 100,
@@ -17,29 +17,29 @@ let menu: menuType = {
 describe('Sum of object properties', () => {
   it('Sum of 1,2,3,4 should return 10', () => {
     const result = sum(1, 2, 3, 4);
-    expect(result).to.equal(10);
+    expect(result).toBe(10);
   });
 });
 
 describe('calculateSum of object properties', () => {
   it('Sum of 100 300 250 should return 650', () => {
     const result = calculateSum(salaries);
-    expect(result).to.equal(650);
+    expect(result).toBe(650);
   });
 });
 
 describe('Find key of max value', () => {
   it('Salaries max value should return Петя', () => {
     const result = findMax(salaries);
-    expect(result).to.equal('Петя');
+    expect(result).toBe('Петя');
   });
 });
 
 describe('Doubles all numeric values of object', () => {
   it('Should return object with all numeric values multiplied at 2', () => {
     const result = multiplyNumeric(menu);
-    expect(result.width).to.equal(400);
-    expect(result.height).to.equal(600);
+    expect(result.width).toBe(400);
+    expect(result.height).toBe(600);
   });
 });
 
@@ -48,8 +48,8 @@ describe('Assume 2 positive numbers greater than 0 will return a array of length
     const a = 1;
     const b = 10;
     const result = countBy(a,b);
-    expect(result.length).to.equal(b);
-    for (var i = 0; i < b; i++) expect(result[i]).to.equal((i+1)*a);
+    expect(result.length).toBe(b);
+    for (var i = 0; i < b; i++) expect(result[i]).toBe((i+1)*a);
   });
 });
 
@@ -60,6 +60,6 @@ describe('Function will add new Class to Object.className if there are no such c
     }
     addClass(obj, 'menu');
     addClass(obj, 'test');
-    expect(obj.className).to.equal('open menu class test');
+    expect(obj.className).toBe('open menu class test');
   });
 });
