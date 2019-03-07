e3710bcb66ca0f1f2ff37ef1334658042a72fa1e
skinenkov <skinenkov@gmail.com>
2019-03-07 12:17:52 +0200

    Ivankov hometask of ts-lesson. (#28)
    * Hometask of ts-lesson.
    
    * tslint requirements

---
+++ src/ivankov.ihor/homework.ts
@@ -0,0 +1,53 @@
+export type NumericValues = { [key: string]: number };
+export type AnyValues = { [key: string]: any };
+export interface IClassName {
+  className: string;
+}
+
+export const isNumeric = function(n: any): boolean {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+export const propertySum = function(values: NumericValues): number {
+    let sum: number = 0;
+    Object.keys(values).forEach(element => {
+      sum += values[element];
+    });
+    return sum;
+  };
+
+export const maxValueProperty = function(values: NumericValues): string {
+  let maxValue: number = 0;
+  let valueName: string = 'нет сотрудников';
+  Object.keys(values).forEach(element => {
+    if (values[element] > maxValue) {
+      maxValue = values[element];
+      valueName = element;
+    }
+  });
+  return valueName;
+};
+
+export const multiplyNumeric = function(values: AnyValues, multiplier: number = 2): void {
+  for (const key in values) {
+    values[key] = isNumeric(values[key]) ? values[key] * multiplier : values[key];
+  }
+};
+
+export const addClass = function(obj: IClassName, newClass: string): void {
+  let currentClasses = obj.className.split(' ');
+  if (currentClasses.indexOf(newClass) === -1) {
+    currentClasses.push(newClass);
+  }
+  obj.className = currentClasses.join(' ');
+};
+
+export const countBy = function(x: number, n: number): number[] {
+  let result: number[] = [];
+  let initial: number = x;
+  for (let ind: number = 0; ind < n; ind++) {
+    result.push(initial);
+    initial += x;
+  }
+  return result;
+};
