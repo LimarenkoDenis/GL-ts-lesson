43ef103a1c00193d87a76d0756b3eda4dd0dee3b
Roman Doschich <roman.doschich@gmail.com>
2019-03-11 13:15:48 +0200

    fix tslint and other comments

--- src/doschich.roman/homework.spec.ts
+++ src/doschich.roman/homework.spec.ts
@@ -1,65 +1,72 @@
-import { sum, calculateSum, findMax, multiplyNumeric, menuType, countBy, addClass, classType } from './homework';
-// import { expect } from 'chai';
-// import 'mocha';
+import { addClass, calculateSum, countBy, findMax, IClassType, IMenuType, multiplyNumeric, sum } from "./homework";
 
 const salaries: { [key: string]: number } = {
-  "Вася": 100,
-  "Петя": 300,
-  "Даша": 250
+  Вася: 100,
+  Даша: 250,
+  Петя: 300,
 };
 
-let menu: menuType = {
-  width: 200,
+const menu: IMenuType = {
   height: 300,
-  title: "My menu"
+  title: "My menu",
+  width: 200,
 };
 
-describe('Sum of object properties', () => {
-  it('Sum of 1,2,3,4 should return 10', () => {
-    const result = sum(1, 2, 3, 4);
+describe("Sum of object properties", () => {
+  it("Sum of 1,2,3,4 should return 10", () => {
+    const result: number = sum(1, 2, 3, 4);
     expect(result).toBe(10);
   });
 });
 
-describe('calculateSum of object properties', () => {
-  it('Sum of 100 300 250 should return 650', () => {
-    const result = calculateSum(salaries);
+describe("calculateSum of object properties", () => {
+  it("Sum of 100 300 250 should return 650", () => {
+    const result: number = calculateSum(salaries);
     expect(result).toBe(650);
   });
 });
 
-describe('Find key of max value', () => {
-  it('Salaries max value should return Петя', () => {
-    const result = findMax(salaries);
-    expect(result).toBe('Петя');
+describe("Find key of max value", () => {
+  it("Salaries max value should return Петя", () => {
+    const result: string = findMax(salaries);
+    expect(result).toBe("Петя");
   });
 });
 
-describe('Doubles all numeric values of object', () => {
-  it('Should return object with all numeric values multiplied at 2', () => {
-    const result = multiplyNumeric(menu);
+describe("Doubles all numeric values of object", () => {
+  it("Should return object with all numeric values multiplied at 2", () => {
+    const result: IMenuType = multiplyNumeric(menu);
     expect(result.width).toBe(400);
     expect(result.height).toBe(600);
   });
 });
 
-describe('Assume 2 positive numbers greater than 0 will return a array of length (n) with multiples of (x).', () => {
-  it('Assume 1, 10 will return [1,2,3,4,5,6,7,8,9,10]', () => {
-    const a = 1;
-    const b = 10;
-    const result = countBy(a,b);
+describe("Assume 2 positive numbers greater than 0 will return an array of length (n) with multiples of (x).", () => {
+  it("Assume 1, 10 will return [1,2,3,4,5,6,7,8,9,10]", () => {
+    const a: number = 1;
+    const b: number = 10;
+    const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const result: number[] = countBy(a, b);
+
+    expect(a).toBeGreaterThan(0);
+    expect(b).toBeGreaterThan(0);
     expect(result.length).toBe(b);
-    for (var i = 0; i < b; i++) expect(result[i]).toBe((i+1)*a);
+    expect(result).toEqual(
+      expect.arrayContaining(expected),
+    );
   });
 });
 
-describe('Function will add new Class to Object.className if there are no such class exist and remove all spaces', () => {
-  it('open   menu    class with addClass("menu") addClass("test") will return "open menu class test"', () => {
-    let obj: classType = {
-      className: 'open   menu    class'
-    }
-    addClass(obj, 'menu');
-    addClass(obj, 'test');
-    expect(obj.className).toBe('open menu class test');
+describe("Function will add new Class to className if there are no such class exist and remove all spaces", () => {
+  it("open   menu    class with addClass('menu') addClass('test') will return 'open menu class test'", () => {
+    const obj: IClassType = {
+      className: "open   menu    class",
+    };
+    const expected = "open menu class test";
+    addClass(obj, "menu");
+    addClass(obj, "test");
+    expect(obj.className).toEqual(
+      expect.stringContaining(expected),
+    );
   });
 });
--- src/doschich.roman/homework.ts
+++ src/doschich.roman/homework.ts
@@ -1,114 +1,116 @@
 export const sum: (...a: number[]) => number
   = (...a: number[]) => a.reduce((acc: number, val: number) => acc + val, 0);
 
-
-
-interface salariesType {
+interface ISalariesType {
   [key: string]: number;
 }
-export interface menuType {
+export interface IMenuType {
   [key: string]: any;
 }
-export interface classType {
+export interface IClassType {
   [key: string]: string;
 }
 
-const salaries: salariesType = {
-  "Вася": 100,
-  "Петя": 300,
-  "Даша": 250
+const salaries: ISalariesType = {
+  Вася: 100,
+  Даша: 250,
+  Петя: 300,
 };
-salaries['Валера'] = 250;
+salaries.Валера = 250;
 
-let menu: menuType = {
-  width: 200,
+const menu: IMenuType = {
   height: 300,
-  title: "My menu"
+  title: "My menu",
+  width: 200,
 };
 
-let obj: classType = {
-  className: 'open   menu    class'
-}
+const element: IClassType = {
+  className: "open   menu    class",
+};
 
 calculateSum(salaries);
 findMax(salaries);
 multiplyNumeric(menu);
-countBy(1,10);
-countBy(2,5);
-addClass(obj, ' menu');
-addClass(obj, '  newClass   ');
+countBy(1, 10);
+countBy(2, 5);
+addClass(element, "  menu");
+addClass(element, "   newClass   ");
 
-export function calculateSum(arg: salariesType): number {
-  let arr: number[] = [];
+export function calculateSum(arg: ISalariesType): number {
+  const arr: number[] = [];
 
-  for (let i in arg) {
-    let current:number = arg[i];
+  for (const i of Object.keys(arg)) {
+    const current: number = arg[i];
     arr.push(current);
   }
 
-  const total:number = arr.reduce((sum:number, current:number) => sum + current, 0);
-  console.log(`${total} total salary`);
+  const total: number = arr.reduce((amount: number, current: number) => amount + current, 0);
   return total;
-};
+}
 
-export function findMax(arg: salariesType): string {
+export function findMax(arg: ISalariesType): string {
   let max: number = 0;
-  let name: string = '';
+  let name: string = "";
 
-  for (let i in arg) {
-    let current:number = arg[i];
+  for (const i of Object.keys(arg)) {
+    const current: number = arg[i];
 
     if (current > max) {
       name = i;
       max = current;
-		}
+    }
   }
 
-  console.log(`${name} has max salary`);
   return name;
-};
+}
 
-export function multiplyNumeric(arg: menuType): menuType {
+export function multiplyNumeric(arg: IMenuType): IMenuType {
   const isNumeric = (n: any): boolean => !isNaN(parseFloat(n)) && isFinite(n);
 
-  for (let i in arg) {
-    if (isNumeric(arg[i])) menu[i] = arg[i] * 2;
+  for (const i of Object.keys(arg)) {
+    if (isNumeric(arg[i])) {
+      menu[i] = arg[i] * 2;
+    }
   }
 
-  console.log(menu);
   return menu;
 }
 
 export function countBy(a: number, b: number) {
-  let arr = [];
-  if (a <= 0 || b <= 0) { // sorry :'(
-    console.error(`Numbers should be positive and not equals to 0. What we have: a=${a}, b=${b}`);
+  const arr = [];
+
+  for (let i = 1; i <= b; i++) {
+    arr[i - 1] = i * a;
   }
-  for (let i = 1; i <= b; i++) arr[i-1] = i*a;
-  console.log(arr);
+
   return arr;
 }
 
-export function addClass(obj: classType, cls: string): object {
-  const arr = obj.className.split(' ');
-  cls = cls.replace(/^\s*/,'').replace(/\s*$/,'');
+export function addClass(obj: IClassType, cls: string): object {
+  const arr = obj.className.split(" ");
+  cls = cls.replace(/^\s*/, "").replace(/\s*$/, "");
 
   for (let i = arr.length - 1; i >= 0; i--) {
-    if (arr[i] === '') arr.splice(i, 1);
+    if (arr[i] === "") {
+      arr.splice(i, 1);
+    }
   }
 
-  if (hasClass(obj, cls) !== true) arr.push(cls);
+  if (hasClass(obj, cls) !== true) {
+    arr.push(cls);
+  }
 
-  obj.className = arr.join(' ');
-  console.log(obj.className);
+  obj.className = arr.join(" ");
   return obj;
 }
 
-function hasClass(obj: classType, cls: string): boolean {
-  const arr = obj.className.split(' ');
-
+function hasClass(obj: IClassType, cls: string): boolean {
+  const arr = obj.className.split(" ");
+  /* tslint:disable:prefer-for-of */
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] === cls) return true;
+    if (arr[i] === cls) {
+      return true;
+    }
   }
 
   return false;
